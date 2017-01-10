9137c01f780f980e4e121073746b50efd9ada53f
Jordan Eddy <jeddy016@gmail.com>
2017-01-09 19:13:42 -0600

    fix scrollTop

--- javascript/script.js
+++ javascript/script.js
@@ -1,17 +1,18 @@
-$(document).ready(function(){ 
-    $(window).scroll(function(){ 
-        if ($(this).scrollTop() > 100) { 
-            $('.scrollToTop').fadeIn(200); 
-        } else { 
-            $('.scrollToTop').fadeOut(200); 
-        } 
+ $(document).ready( function() { 
+    $(window).scroll( function() { 
+    	if($(this).scrollTop() < 300) {
+    		$('.scrollToTop').fadeOut(300);
+    }
+    	else {
+    		$('.scrollToTop').fadeIn(300);
+    	}
     });
 
-    $(.scrollToTop).click(function(){ 
+    $('.scrollToTop').click( function(){ 
         $('html, body').animate({ scrollTop: 0 }, 600); 
         return false; 
-    });
-});
+    }); 
+}); 
 
 
 
